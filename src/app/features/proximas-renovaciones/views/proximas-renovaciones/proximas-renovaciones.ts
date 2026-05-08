0b2bd82a2d0a15228c60b36ac5458c5162f041bc
lausnchez <lausnchezmartn@gmail.com>
2026-05-08 13:28:19 +0200

    Agregar componente de paginacion de materials

--- src/app/features/proximas-renovaciones/views/proximas-renovaciones/proximas-renovaciones.ts
+++ src/app/features/proximas-renovaciones/views/proximas-renovaciones/proximas-renovaciones.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PrHeader } from '../../components/pr-header/pr-header';
 import { PrRenovationsTable } from '../../components/pr-renovations-table/pr-renovations-table';
 import { PolizaRow } from '../../../../shared/components/poliza-row/poliza-row';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 @Component({
   selector: 'app-proximas-renovaciones',
-  imports: [PrHeader, PrRenovationsTable, PolizaRow],
+  imports: [PrHeader, PrRenovationsTable, MatPaginatorModule],
   templateUrl: './proximas-renovaciones.html',
   styleUrl: './proximas-renovaciones.scss',
 })
